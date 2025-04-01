1b33c7c691d5e4452849b3b035e05a5ebca1b8af
3rumalarao <3rumalarao@gmail.com>
2025-03-31 23:45:23 -0500

    Create main.tf

---
+++ efs/main.tf
@@ -0,0 +1,16 @@
+resource "aws_efs_file_system" "this" {
+  performance_mode = var.efs.performance_mode
+  encrypted        = var.efs.encrypted
+
+  tags = merge(var.common_tags, {
+    Name = "${var.environment}-efs"
+  })
+}
+
+resource "aws_efs_mount_target" "this" {
+  for_each = toset(var.private_subnets)
+
+  file_system_id  = aws_efs_file_system.this.id
+  subnet_id       = each.value
+  security_groups = var.efs_security_groups
+}
