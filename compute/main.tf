e581a427eb6ab6a69fdfc871566c8c17a4a11492
3rumalarao <3rumalarao@gmail.com>
2025-04-01 18:41:31 -0500

    Update main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -1,4 +1,17 @@
-# Private Servers (Non-AZ Spread)
+locals {
+  # Flatten the applications map to create two instances per application.
+  app_instances = flatten([
+    for app_key, app in var.applications : [
+      for i in range(2) : {
+        app_key         = app_key,
+        index           = i,
+        instance_config = app
+      }
+    ]
+  ])
+}
+
+# Private Servers
 resource "aws_instance" "private_servers" {
   for_each = var.private_servers
 
@@ -16,27 +29,26 @@ resource "aws_instance" "private_servers" {
   })
 }
 
-# Application Servers (AZ Spread: 2 per application)
+# Application Servers
 resource "aws_instance" "applications" {
-  for_each = var.applications
-  count    = 2
+  for_each = { for item in local.app_instances : "${item.app_key}-${item.index}" => item }
 
-  ami           = each.value.ami_ids[0]
-  instance_type = each.value.instance_type
-  subnet_id     = each.value.subnet_ids[count.index]
-  key_name      = each.value.key_name
+  ami           = each.value.instance_config.ami_ids[0]
+  instance_type = each.value.instance_config.instance_type
+  subnet_id     = each.value.instance_config.subnet_ids[each.value.index]
+  key_name      = each.value.instance_config.key_name
 
-  vpc_security_group_ids = [for sg in each.value.security_groups : var.vpc_security_group_ids[sg]]
+  vpc_security_group_ids = [for sg in each.value.instance_config.security_groups : var.vpc_security_group_ids[sg]]
   iam_instance_profile   = var.common_iam_role != "" ? var.common_iam_role : null
 
   tags = merge(var.common_tags, {
-    Name   = "${each.key}-${count.index + 1}",
+    Name   = "${each.value.app_key}-${each.value.index + 1}",
     Type   = "Application",
-    Domain = each.value.domain_name
+    Domain = each.value.instance_config.domain_name
   })
 }
 
-# Public Instances (Non-AZ Spread)
+# Public Instances
 resource "aws_instance" "public_instances" {
   for_each = var.public_instances
 
@@ -55,4 +67,3 @@ resource "aws_instance" "public_instances" {
     Type = "Public"
   })
 }
- 
