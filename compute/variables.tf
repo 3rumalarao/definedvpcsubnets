93ea67c8f9d9f7e1be57f209e35b54417c8de7fc
3rumalarao <3rumalarao@gmail.com>
2025-03-31 23:44:39 -0500

    Create variables.tf

---
+++ compute/variables.tf
@@ -0,0 +1,69 @@
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "selected_private_subnet" {
+  description = "Selected private subnet"
+  type        = string
+}
+
+variable "selected_public_subnet" {
+  description = "Selected public subnet"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "private_servers" {
+  description = "Map of private server configurations"
+  type = map(object({
+    ami_ids         = list(string)
+    instance_type   = string
+    security_groups = list(string)
+    key_name        = string
+    subnet_id       = string
+  }))
+}
+
+variable "applications" {
+  description = "Map of application configurations"
+  type = map(object({
+    ami_ids         = list(string)
+    instance_type   = string
+    security_groups = list(string)
+    domain_name     = string
+    key_name        = string
+    subnet_ids      = list(string)
+  }))
+}
+
+variable "public_instances" {
+  description = "Map of public instance configurations"
+  type = map(object({
+    ami_ids         = list(string)
+    instance_type   = string
+    security_groups = list(string)
+    key_name        = string
+    subnet_id       = string
+  }))
+}
+
+variable "common_iam_role" {
+  description = "IAM role to attach to instances"
+  type        = string
+}
+
+variable "common_tags" {
+  description = "Common tags"
+  type        = map(string)
+}
+
+# Renamed variable to reflect latest naming conventions.
+variable "vpc_security_group_ids" {
+  description = "Mapping of security group names to their IDs"
+  type        = map(string)
+}
