01e824aedab6ccb6f228277490bde54cadd28b6e
3rumalarao <3rumalarao@gmail.com>
2025-03-31 23:45:45 -0500

    Create variables.tf

---
+++ efs/variables.tf
@@ -0,0 +1,30 @@
+variable "vpc_id" {
+  type = string
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "efs" {
+  description = "EFS configuration"
+  type = object({
+    performance_mode = string
+    encrypted        = bool
+    efs_mount_points = list(string)
+  })
+}
+
+variable "common_tags" {
+  type = map(string)
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "efs_security_groups" {
+  description = "Security groups for EFS mount targets"
+  type        = list(string)
+  default     = []
+}
