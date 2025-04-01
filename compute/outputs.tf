9bc0a9c71fd7d4301d7c5b9e1d65298c2549300d
3rumalarao <3rumalarao@gmail.com>
2025-03-31 23:58:38 -0500

    Update outputs.tf

--- compute/outputs.tf
+++ compute/outputs.tf
@@ -5,3 +5,13 @@ output "instances" {
     public_instances = aws_instance.public_instances
   }
 }
+
+output "application_instances" {
+  description = "Mapping of application name to list of instance IDs"
+  value = {
+    for app in keys(var.applications) : app =>
+    [
+      for i in range(0,2) : aws_instance.applications[app][i].id
+    ]
+  }
+}
